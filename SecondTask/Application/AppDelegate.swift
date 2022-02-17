6ad53ccb473adf291dc1dec09aa86f227f4a0ba8
Bektemirov Shahzod <ioshakh27@gmail.com>
2022-02-18 03:19:14 +0500

    pod install and create first controller

---
+++ SecondTask/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SecondTask
+//
+//  Created by Shakhzod Bektemirov on 2022/02/18.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+   
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+    private func setDelegateView(){
+        
+    }
+}
---
+++ SecondTask/Scenes/FirstVC.swift
@@ -0,0 +1,16 @@
+//
+//  FirstVC.swift
+//  SecondTask
+//
+//  Created by Shakhzod Bektemirov on 2022/02/18.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
